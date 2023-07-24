3305ea67ea85639681b9f4ef8277e9caee60aa61
Samiul <asamiul101@gmail.com>
2023-07-24 21:37:53 +0100

    data insertion for dogs and owners table

---
+++ java-api/src/main/resources/data_insertion_for_dogs_Samiul.sql
@@ -0,0 +1,9 @@
+insert into dogs
+    (name, age)
+values
+    ('Tommy', 4),
+    ('Jacob', 2),
+    ('Jojo', 1),
+    ('Dumbo', 3),
+    ('Ally', 1)
+;
---
+++ java-api/src/main/resources/data_insertion_for_owners_Samiul.sql
@@ -0,0 +1,4 @@
+insert into owners
+    (name)
+values
+    210, 222, 232, 234, 231;
